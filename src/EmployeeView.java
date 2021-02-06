59c05e567e751c11b3c48c1316e7ff7a6438f9c7
Oshadi Ranathunge <45248856+oshadiranathunge@users.noreply.github.com>
2021-02-06 23:52:08 +0530

    Update EmployeeView.java

--- src/EmployeeView.java
+++ src/EmployeeView.java
@@ -3,6 +3,6 @@ public class EmployeeView {
 	public void printDetails(String studentName, String studentRollNo) {
 		System.out.println("Student");
 		System.out.println("Name:" +studentName);
-		System.out.println("Roll No:" +studentRollNo);
+		System.out.println("Roll No for student:" +studentRollNo);
 	}
 }
